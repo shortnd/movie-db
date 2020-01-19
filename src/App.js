a608923cdc05e07630c4defdd0419444734b9195
Collin <ococncol@gmail.com>
2020-01-19 17:23:14 -0500

    added eslint

--- src/App.js
+++ src/App.js
@@ -4,38 +4,34 @@ import './App.css';
 
 import Movie from './Movie';
 
-const movies = [{
-    id: 1,
-    title: 'Star Wars',
-    desc: 'A space movie'
-  }, {
-    id: 2,
-    title: 'Spider Man'
-  }, {
-    id: 3,
-    title: '36th Chamber of Shaolin'
-  }
-];
-
 class App extends Component {
+  state = {
+    movies: [],
+  }
 
   async componentDidMount() {
     try {
-      const res = await fetch()
+      const res = await fetch('https://api.themoviedb.org/3/discover/movie?api_key=a941b96952ba6330c47874e72e8bc4b0&language=en-US&sort_by=popularity.desc&include_adult=false&include_video=false&page=1');
+      const movies = await res.json();
+      this.setState({
+        movies: movies.results,
+      });
     } catch (e) {
-      //
+      // eslint-disable-next-line no-console
+      console.log(e);
     }
   }
 
   render() {
+    const { movies } = this.state;
     return (
       <div className="App">
         <header className="App-header">
-          <img src={logo} className="App-logo" alt="logo"/>
+          <img src={logo} className="App-logo" alt="logo" />
         </header>
-        {movies.map(movie => <Movie movie={movie} desc={movie.desc} key={movie.id} /> )}
+        {movies.map((movie) => <Movie movie={movie} desc={movie.desc} key={movie.id} />)}
       </div>
-    )
+    );
   }
 }
 
--- src/Movie.js
+++ src/Movie.js
@@ -1,24 +1,22 @@
-import React, { Component } from 'react'
-import PropTypes from "prop-types";
+import React, { Component } from 'react';
+import PropTypes from 'prop-types';
+
 
 export default class Movie extends Component {
   static propTypes = {
     movie: PropTypes.shape({
       title: PropTypes.string.isRequired,
     }),
-    desc: PropTypes.string
-  }
-
-  static defaultProps = {
-    desc: 'Description not avaliable'
+    desc: PropTypes.string,
   }
 
   render() {
+    const { movie, desc } = this.props;
     return (
       <div>
-        <h3>{this.props.movie.title}</h3>
-        <p>{this.props.desc}</p>
+        <h3>{movie.title}</h3>
+        <p>{desc}</p>
       </div>
-    )
+    );
   }
 }
